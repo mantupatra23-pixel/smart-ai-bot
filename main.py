602c622f1ab9f1896b1697ee7d89b9a7dc86ab17
mantupatra23-pixel <mantupatra23@gmail.com>
2026-02-22 21:32:48 +0530

    fixed gemini model

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import os
 
 app = FastAPI()
 
-# Load API key
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 class ChatRequest(BaseModel):
     message: str
